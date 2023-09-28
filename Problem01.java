4d7217bebb0ae35edd07dfb2a3b09e53a1ab59e9
S <s>
2023-09-28 17:39:03 +0400

    {"types":"C","runs":2}

--- Problem01.java
+++ Problem01.java
@@ -1,5 +1,6 @@
 import stanford.karel.Karel;
-
+//კარელი დგას 1x1 უჯრაზე. 3x1 უჯრაზე დევს ბრილიანტი, კარელმა ბრილიანტი უნდა
+//გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა.
 public class Problem01 extends Karel{
 	public void run (){
 		move();
