490f222fc52e596b0f31b42395d7e46c15e1bcd7
S <s>
2023-10-03 11:40:41 +0400

    {"types":"C","runs":3}

--- Problem08.java
+++ Problem08.java
@@ -1,4 +1,13 @@
+import stanford.karel.SuperKarel;
 
-public class Problem08 {
+public class Problem08 extends SuperKarel {
+	//კარელმა უნდა გამოაკლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
+	//m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n-m ბრილიანტი. ჩათვალეთ, რომ
+	//კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს და ჩათვალეთ, რომ n>=m.
+	
+	
+	public void run(){
+		
+	}
 
 }
