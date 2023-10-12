c9f3ad3096e6b0a7a3df481e27fba36e14cf2d54
S <s>
2023-10-12 17:15:22 +0400

    {"types":"C","runs":4}

--- Problemhome.java
+++ Problemhome.java
@@ -3,6 +3,23 @@ import acm.program.GraphicsProgram;
 
 public class Problemhome extends GraphicsProgram{
 	public void run(){
+		int a = 5;
+		a = a + 1;
+	
+		if(2 == 2) {
+			double b = 3;
+			b = 4;
+			b = a + 3;
+			
+			GLabel label = new GLabel ("a :" + a);
+		}
+		
+	
 		
 	}
+	
+	private void testVoid() {
+		int a = 7;
+	}
+	
 }
