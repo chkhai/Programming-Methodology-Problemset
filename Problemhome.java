d67ba9cd3744acfe25f6895399fedf8c0940ecd1
S <s>
2023-10-12 17:05:28 +0400

    {"types":"C","runs":4}

--- Problemhome.java
+++ Problemhome.java
@@ -3,13 +3,6 @@ import acm.program.GraphicsProgram;
 
 public class Problemhome extends GraphicsProgram{
 	public void run(){
-		GRect outline = new GRect(160, 160, 400, 200);
-		add(outline);
-		GLine line = new GLine(160, 160, 360, 80);
-		add(line);
-		GLine SecondLine = new GLine(560, 160, 360, 80);
-		add(SecondLine);
-		GRect door = new GRect(180, 220, 80, 40);
-		add(door);
+		
 	}
 }
