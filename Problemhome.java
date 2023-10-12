00ed25ec4284129dd09450de97bac79d8415574c
S <s>
2023-10-12 17:33:58 +0400

    {"types":"C","runs":12}

--- Problemhome.java
+++ Problemhome.java
@@ -3,7 +3,10 @@ import acm.program.GraphicsProgram;
 
 public class Problemhome extends GraphicsProgram{
 	//constant
-	private static final double HOUSE_WIDTH = 3.14;
+	private static final double HOUSE_WIDTH = 200;
+	private static final double HOUSE_HEIGHT = 300;
+	private static final double DOOR_WIDTH = 40;
+	private static final double DOOR_HEIGHT = 60;
 	
 	public void run(){
 		drawMidLine();
