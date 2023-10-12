a2526699d2e8c1556744bca7e47a47f32b19cbd4
S <s>
2023-10-12 17:25:42 +0400

    {"types":"C","runs":11}

--- Problemhome.java
+++ Problemhome.java
@@ -8,7 +8,7 @@ public class Problemhome extends GraphicsProgram{
 
 	private void drawMidLine() {
 		double x1 = 0;
-		double y1 = getHeight / 2;
+		double y1 = getHeight() / 2;
 		double x2 = getWidth();
 		double y2 = y1;
 		Gline line = new GLine(x1, y1, x2, y2);
