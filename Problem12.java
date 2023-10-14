35d8cd738bd794efdae93d5b33f1da39b861afc9
S <s>
2023-10-14 21:43:45 +0400

    {"types":"C","runs":9}

--- Problem12.java
+++ Problem12.java
@@ -8,8 +8,9 @@ public class Problem12 extends GraphicsProgram {
 	public void run(){
 		GRect rect = new GRect(150, 100, 300, 300);
 		add(rect);
-		GLine firstline = new GLine (300,50, 150, 100);
+		GLine firstline = new GLine (300, 50, 150, 100);
 		add(firstline);
+		GLine seconline = new GLine (300, 50, 450, 100)
 		
 	}
 }
