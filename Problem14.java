3fce6c536c543570cf3045d367c13d953ff05beb
S <s>
2023-10-14 22:26:15 +0400

    {"types":"C","runs":32}

--- Problem14.java
+++ Problem14.java
@@ -10,8 +10,8 @@ public class Problem14 extends GraphicsProgram{
 	private void drawRobot() {
 		double x1 = getHeight() / 2 - 100;
 		double y1 = getWidth() / 2 - 100;
-		double x2 = 400;
-		double y2 = 400;
+		double x2 = 200;
+		double y2 = 200;
 		GRect face = new GRect(x1, y1, x2, y2);
 		add(face);
 	}
