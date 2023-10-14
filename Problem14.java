23c222c618252838c2f7f5603d0c0f408b8a985b
S <s>
2023-10-14 23:53:16 +0400

    {"types":"C","runs":52}

--- Problem14.java
+++ Problem14.java
@@ -20,8 +20,8 @@ public class Problem14 extends GraphicsProgram{
 	//}
 
 	private void drawLeftEye() {
-		double x1 =getHeight() / 2 - 50;
-		double y1 =getWidth() /2 - 200;
+		double x1 =getHeight() / 2 - 75;
+		double y1 =getWidth() /2 - 250;
 		double x2 =50;
 		double y2 =50;
 		GRect eye = new GRect(x1, y1, x2, y2);
