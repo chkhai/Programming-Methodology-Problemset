aa10d2a14240e5abeb46c17bc997e61b6aa78934
S <s>
2023-10-15 00:21:41 +0400

    {"types":"C","runs":66}

--- Problem16.java
+++ Problem16.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Problem16 {
-
+public class Problem16 extends ConsoleProgram{
+	
+	public void run() {
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
+		int c = readInt("Enter c: ");
+		double harmonicaverage = b * c * a / b * c + c * a + a * b;
+		println("Harmonic average is " + harmonicaverage + ".");
+	}
 }
