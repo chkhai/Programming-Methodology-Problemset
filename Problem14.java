71da03c699ac2752ff11611820cf25a8b77216fb
S <s>
2023-10-14 22:24:30 +0400

    {"types":"C","runs":30}

--- Problem14.java
+++ Problem14.java
@@ -1,4 +1,17 @@
+import acm.program.GraphicsProgram;
 
-public class Problem14 {
+public class Problem14 extends GraphicsProgram{
+	
+	public void run() {
+		drawRobot();
+	}
 
+	private void drawRobot() {
+		double x1 = getHeight() / 2 - 200;
+		double y1 = getWIdth () / 2 -200;
+		double x2 = 400;
+		double y2 = 400;
+		GRect face = new GRect(x1, y1, x2, y2);
+		add(face);
+	}
 }
