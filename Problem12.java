c5b409c6934e8a0230e7edb3ab817535d9b57cd6
S <s>
2023-10-14 21:49:09 +0400

    {"types":"C","runs":18}

--- Problem12.java
+++ Problem12.java
@@ -8,9 +8,9 @@ public class Problem12 extends GraphicsProgram {
 	public void run(){
 		GRect house = new GRect(150, 100, 300, 300);
 		add(house);
-		GLine firstline = new GLine (300, 50, 150, 100);
+		GLine firstline = new GLine (300, 30, 150, 100);
 		add(firstline);
-		GLine secondline = new GLine (300, 50, 450, 100);
+		GLine secondline = new GLine (300, 30, 450, 100);
 		add(secondline);
 		GRect door = new GRect (250, 250, 100, 150);
 		add(door);
