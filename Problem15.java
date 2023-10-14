f2a0e3cda8243a0a87c8d78c63c1e6dfdc03f1be
S <s>
2023-10-15 00:14:17 +0400

    {"types":"C","runs":62}

--- Problem15.java
+++ Problem15.java
@@ -4,9 +4,9 @@ public class Problem15 extends ConsoleProgram{
 	
 	public void run() {
 		println("This program calculates the arithmetic average of two numbers");
-		double a = readInt("Enter a:");
-		double b = readInt("Enter b:");
-		double arithmetic average = a / 2 + b / 2;
+		int a = readInt("Enter a:");
+		int b = readInt("Enter b:");
+		int arithmetic average = a / 2 + b / 2;
 		println("Arithmetic average is" + arithmetic average + ".");
 	}
 }
