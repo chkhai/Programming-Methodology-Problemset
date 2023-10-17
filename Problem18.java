c038c4da2da617f644e237fca54f2b2570626a34
S <s>
2023-10-17 17:51:39 +0400

    {"types":"C","runs":10}

--- Problem18.java
+++ Problem18.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class Problem18 {
-
+public class Problem18 extends ConsoleProgram{
+	//კონსოლიდან წაიკითხეთ ორი მთელი რიცხვი და დაბეჭდეთ მათ შორის მაქსიმალური.
+	
+	public void run() {
+		double a = readInt("Enter real number: ");
+		double b = readInt("Enter real number: ");
+		if(a > b){
+			println(a);
+		}
+	}
 }
