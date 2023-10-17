e9e44846c1144401deb19ee391979e9ef5be6353
S <s>
2023-10-17 17:58:36 +0400

    {"types":"C","runs":15}

--- Problem19.java
+++ Problem19.java
@@ -5,6 +5,9 @@ public class Problem19 extends ConsoleProgram{
 //კონსოლიდან წაიკითხეთ მთელი რიცხვი n და შემდეგ n ჯერ დაბეჭდეთ “hello ” + i, სადაც i
 //	იცვლება 0 დან n-1 მდე.
 	public void run() {
-		double 
+		double n = readInt("Enter real number");
+		for(n=0; n<n+1; n++){
+			println("hello");
+		}
 	}
 }
