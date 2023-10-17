dc4e21bfb415745d0fb0a3ecbda363162de0b9eb
S <s>
2023-10-17 17:57:02 +0400

    {"types":"C","runs":15}

--- Problem19.java
+++ Problem19.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class Problem19 {
-
+public class Problem19 extends ConsoleProgram{
+	
+//კონსოლიდან წაიკითხეთ მთელი რიცხვი n და შემდეგ n ჯერ დაბეჭდეთ “hello ” + i, სადაც i
+//	იცვლება 0 დან n-1 მდე.
+	public void run() {
+		double 
+	}
 }
