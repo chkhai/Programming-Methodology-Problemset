df602b1294ef901e96ca2b0a172b3d7ec3ec96e5
S <s>
2023-10-19 17:34:33 +0400

    {"types":"C","runs":6}

--- Problem20.java
+++ Problem20.java
@@ -4,11 +4,12 @@ public class Problem20 extends ConsoleProgram {
 	//კონსოლიდან წაიკითხეთ n, შემდეგ წაიკითხეთ n ცალი მთელი რიცხვი და დაბეჭდეთ ჯამი
 	public void run() {
 		int n = readInt("Enter real number :");
-	
+		int sum = 0;
 		for (int i = 0; i < n; i++) {
 			int num = readInt("Enter number #" + i + ": ");
 			
 			println(num);
+			sum += num;
 		}
 	}
 }
