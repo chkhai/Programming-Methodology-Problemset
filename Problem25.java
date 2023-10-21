6fd070fc6ca950472b7883bea0e02c436b5872c6
S <s>
2023-10-21 15:15:04 +0400

    {"types":"C","runs":37}

--- Problem25.java
+++ Problem25.java
@@ -1,4 +1,11 @@
-
-public class Problem25 {
-
+import acm.program.ConsoleProgram;
+//მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+//უმცირესი საერთო ჯერადი.
+public class Problem25 extends ConsoleProgram{
+	public void run() {
+		int a = readInt("Enter real number: ");
+		int b = readInt("Enter real number: ");
+		int lcm = a*b;
+		println(lcm);
+	}
 }
