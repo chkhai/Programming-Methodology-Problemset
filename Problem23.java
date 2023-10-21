51a9b3e3c7577d41774b55d553d2dbc95fd26138
S <s>
2023-10-21 14:22:53 +0400

    {"types":"C","runs":1}

--- Problem23.java
+++ Problem23.java
@@ -1,4 +1,12 @@
-
-public class Problem23 {
-
+import acm.program.ConsoleProgram;
+//მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს 1 დან n მდე რიცხვების
+//ჯამი
+public class Problem23 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("Enter real number: ");
+		int sum = 0;
+		for(int i = 0; i < n; i++){
+			sum = sum + i;
+		}
+	}
 }
