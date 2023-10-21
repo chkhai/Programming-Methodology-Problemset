10ab6e4555676a76a0a06a7910c93e70fa7c2387
S <s>
2023-10-21 14:18:00 +0400

    {"types":"C","runs":1}

--- Problem22.java
+++ Problem22.java
@@ -4,15 +4,13 @@ public class Problem22 extends GraphicsProgram{
 	private static final int NUM_ROWS = 8;
 	private static final int NUM_COLUMNS = 8;
 	public void run() {
-		for(i = 0; i < NUM_ROWS; i++){
-			drawHorisontalPart();
+		for(int i = 0; i < NUM_ROWS; i++){
+			drawHorisontalPart(i);
 		}
+	}
+	private void drawHorisontalPart(int i) {		
 	}
 
-	private void drawHorisontalPart() {
-		for(j = 0; j < NUM_COLUMNS; j++){
-			GRect rect = new GRect (getHeight);
-		}
+	
 		
-	}
 }
