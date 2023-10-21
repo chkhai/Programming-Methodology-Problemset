21abb0e57e0d3ee7d202fa7b71f7644a21614313
S <s>
2023-10-21 14:12:06 +0400

    {"types":"C","runs":1}

--- Problem22.java
+++ Problem22.java
@@ -1,7 +1,18 @@
 import acm.program.GraphicsProgram;
 
 public class Problem22 extends GraphicsProgram{
+	private static final int NUM_ROWS = 8;
+	private static final int NUM_COLUMNS = 8;
 	public void run() {
+		for(i = 0; i < NUM_ROWS; i++){
+			drawHorisontalPart();
+		}
+	}
+
+	private void drawHorisontalPart() {
+		for(j = 0; j < NUM_COLUMNS; j++){
+			GRect rect = new GRect (getHeight);
+		}
 		
 	}
 }
