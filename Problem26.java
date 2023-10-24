07d7d0ce2e4cf4c2cb2c6577344bf8f948d7b21f
S <s>
2023-10-24 11:43:23 +0400

    {"types":"C","runs":0}

--- Problem26.java
+++ Problem26.java
@@ -2,6 +2,22 @@ import acm.program.ConsoleProgram;
 
 public class Problem26 extends ConsoleProgram{
 	public void run() {
-		
+		int a = readInt("Enter integer :");
+		int b = readInt("Enter integer :");
+		int k = gcd(a, b);
+	}
+
+	private int gcd(int a, int b) {
+		while(a != 0 && b != 0){
+			if(a > b){
+				a %= b;
+			}else{
+				b %= a;
+			}
+		}
+		if(a > b){
+			return a;
+		}
+		return b;
 	}
 }
