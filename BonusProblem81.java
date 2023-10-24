8c38eec43eaad044e0e5cbea76404e36a27fcb40
S <s>
2023-10-24 17:13:25 +0400

    {"types":"C","runs":1}

--- BonusProblem81.java
+++ BonusProblem81.java
@@ -1,4 +1,9 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class BonusProblem81 {
-
+public class BonusProblem81 extends GraphicsProgram{
+	public void run() {
+		GLabel london = new GLabel("london", 0, getHeight());
+		add(london);
+	}
 }
