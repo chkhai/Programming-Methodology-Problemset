1c98b4688ee0af549f8d3451aecc21f648e98ba5
S <s>
2023-10-26 17:14:28 +0400

    {"types":"C","runs":4}

--- Problem28.java
+++ Problem28.java
@@ -4,12 +4,6 @@ public class Problem28 extends ConsoleProgram{
 	private static final int q = -1;
 	
 	public void run() {
-		int a = readInt("Enter integer: ");
-		int count = 0;
-		while(a != q){
-			if(a % 2 == 0) {
-				count +=1;
-			}
-		}
+		while()
 	}
 }
