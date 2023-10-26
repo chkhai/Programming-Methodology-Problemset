3f0654990382c24e25e85fe30bff889bd258076e
S <s>
2023-10-26 17:21:13 +0400

    {"types":"C","runs":4}

--- Problem28.java
+++ Problem28.java
@@ -4,6 +4,15 @@ public class Problem28 extends ConsoleProgram{
 	private static final int q = -1;
 	
 	public void run() {
-		
+		int sum = 0;
+		while(true){
+			int n = readInt("Enter integer: ");
+			if(n % 2 ==0){
+				sum += sum;
+			}
+			if(n = q){
+				break;
+			}
+		}
 	}
 }
