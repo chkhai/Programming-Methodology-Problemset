eb0b860272a9e7ee9a6a6a5144e80519fe3f1535
S <s>
2023-10-26 17:43:23 +0400

    {"types":"C","runs":9}

--- Problem27.java
+++ Problem27.java
@@ -5,14 +5,11 @@ public class Problem27 extends ConsoleProgram{
 		int n = readInt("Enter integer: ");
 		int m = readInt("Enter integer: ");
 		int k = 1;
-		power(n, m, k);
-		println("n powered by n is " + k);
-	}
-
-	private int power(int n, int m, int k) {
 		for(int i = 0; i < m; i++) {
 			k = k * n;
 		}
-		return k;
+		println("n powered by n is " + k);
 	}
+
+		
 }
