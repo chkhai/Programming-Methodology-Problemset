dca1481e7b37939ecad79dccfc3a7883edb487ca
S <s>
2023-10-26 17:08:00 +0400

    {"types":"C","runs":0}

--- Problem27.java
+++ Problem27.java
@@ -1,4 +1,12 @@
-
-public class Problem27 {
-
+import acm.program.ConsoleProgram;
+//მომხმარებელს შეყავს n და m დაბეჭდეთ n^m
+public class Problem27 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("Enter integer: ");
+		int m = readInt("Enter integer: ");
+		int k = 1;
+		for(int i = 0; i < m; i++) {
+			k = k * n;
+		}
+	}
 }
