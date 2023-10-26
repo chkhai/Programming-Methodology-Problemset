119c01622e07f7d7c3c0a632ad1c183abb4c5b50
S <s>
2023-10-26 17:40:08 +0400

    {"types":"C","runs":7}

--- Problem29.java
+++ Problem29.java
@@ -1,5 +1,13 @@
 import acm.program.ConsoleProgram;
 
 public class Problem29 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("Enter integer: ");
+		countTheNumberFromFibonacci(n);
+	}
+
+	private void countTheNumberFromFibonacci(int n) {
+		
+	}
 
 }
