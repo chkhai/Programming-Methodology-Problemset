5eec74651f02043ec625548c363dafe40f188a62
S <s>
2023-10-29 11:30:22 +0400

    {"types":"C","runs":0}

--- TargetProblem.java
+++ TargetProblem.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
-public class TargetProblem {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
+public class TargetProblem extends GraphicsProgram{
+	
+	public void run() {
+		drawFirstCircle();
+	//	drawSecondCircle();
+	//	drawThirdCircle();
+	}
+
+	private void drawFirstCircle() {
+		GOval firstCircle = new GOval(getWidth() - 72, getHeight() - 72, 144, 144);
+		firstCircle.setFilled(true);
+		firstCircle.setColor(Color.RED);
+		add(firstCircle);
+	}
 }
