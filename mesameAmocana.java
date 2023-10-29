72596eb9c75ab5223a690c58de1d82965a8ef5e9
S <s>
2023-10-29 13:01:27 +0400

    {"types":"C","runs":3}

--- mesameAmocana.java
+++ mesameAmocana.java
@@ -1,4 +1,11 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
 public class mesameAmocana extends GraphicsProgram{
+	private static final int RECT_HEIGHT = 12;
+	private static final int RECT_WIDTH = 30;
 
+	public void run() {
+		GLabel program = new GLabel("Program");
+	}
 }
