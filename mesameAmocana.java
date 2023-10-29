1aa6be251fd093a7dd1640119fb296bcd92a7145
S <s>
2023-10-29 20:23:14 +0400

    {"types":"C","runs":31}

--- mesameAmocana.java
+++ mesameAmocana.java
@@ -4,7 +4,8 @@ import acm.program.GraphicsProgram;
 public class mesameAmocana extends GraphicsProgram{
 	private static final int RECT_HEIGHT = 80;
 	private static final int RECT_WIDTH = 200;
-	private static final int DIFFERENCE_BETWEEN_RECTS = 30;
+	private static final int WIDTH_DIFFERENCE_BETWEEN_RECTS = 30;
+	private static final int HEIGHT_DIFFERENCE_BETWEEN_RECTS = 40;
 
 	public void run() {
 		drawConsoleProgramRectAndLabel();
@@ -14,11 +15,18 @@ public class mesameAmocana extends GraphicsProgram{
 	}
 
 	private void drawProgramRectAndLabel() {
-		
+		double x = getWidth() / 2 - RECT_WIDTH / 2;
+		double y = getHeight() / 2 - HEIGHT_DIFFERENCE_BETWEEN_RECTS - RECT_HEIGHT;
+		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+		add(rect);
+		GLabel label = new GLabel("Program");
+		double m = x + RECT_WIDTH / 2 - label.getWidth() / 2;
+		double n = y + RECT_HEIGHT / 2 + label.getAscent() / 2;
+		add(label, m, n);
 	}
 
 	private void drawDialogProgramRectAndLabel() {
-		double x = getWidth() / 2 + RECT_WIDTH / 2 + DIFFERENCE_BETWEEN_RECTS;
+		double x = getWidth() / 2 + RECT_WIDTH / 2 + WIDTH_DIFFERENCE_BETWEEN_RECTS;
 		double y = getHeight() / 2;
 		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
 		add(rect);
@@ -30,7 +38,7 @@ public class mesameAmocana extends GraphicsProgram{
 	}
 
 	private void drawGraphicsProgramRectAndLabel() {
-		double x = getWidth() / 2 - 3 * RECT_WIDTH / 2 - DIFFERENCE_BETWEEN_RECTS;
+		double x = getWidth() / 2 - 3 * RECT_WIDTH / 2 - WIDTH_DIFFERENCE_BETWEEN_RECTS;
 		double y = getHeight() / 2;
 		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
 		add(rect);
