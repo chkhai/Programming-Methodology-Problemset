0b655915a7fe41f93b258faf52857f61cba64769
S <s>
2023-10-29 12:14:59 +0400

    {"types":"C","runs":18}

--- meekvseAmocana.java
+++ meekvseAmocana.java
@@ -7,10 +7,10 @@ public class meekvseAmocana extends ConsoleProgram{
 		while(true){
 			if(n % 2 ==0){
 				n = n / 2;
-				println(n + "is even so I take half: " + n);
+				println(n + " is even so I take half: " + n);
 			}else{
 				n = 3 * n + 1;
-				println(n + "is odd, so I make 3n+1:" + n);
+				println(n + " is odd, so I make 3n+1:" + n);
 			}
 			if(n == 1){
 				break;
