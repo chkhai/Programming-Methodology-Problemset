2a1d1cac808decb981affa32fbde7c54f3f4fff5
S <s>
2023-10-29 16:18:33 +0400

    {"types":"C","runs":1}

--- pirveliAmocana.java
+++ pirveliAmocana.java
@@ -1,10 +1,24 @@
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 //not finished
 public class pirveliAmocana extends GraphicsProgram{
-	private static final int BRICK_WIDHT = 30;
+	private static final int BRICK_WIDTH = 30;
 	private static final int BRICK_HEIGHT = 12;
 	private static final int BRICKS_IN_BASE= 14;
 
 	public void run() {
+		for(int i =0; i < BRICKS_IN_BASE; i++) {
+			drawHorisontalLineOfBricks(i);
+		}
+	}
+
+	private void drawHorisontalLineOfBricks(int i) {
+		for(int j = 0; j < BRICKS_IN_BASE; j++) {
+			int x = getWidth() / 2 - BRICK_WIDTH * j;
+			int y = getHeight() - BRICK_HEIGHT * i;
+			GRect brick = new GRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
+			add(brick);
+		}
+		
 	}
 }
