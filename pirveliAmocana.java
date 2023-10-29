eff6e12ea169d02fe4a45bbdf1400ba2f509c6d6
S <s>
2023-10-29 12:39:56 +0400

    {"types":"C","runs":25}

--- pirveliAmocana.java
+++ pirveliAmocana.java
@@ -1,4 +1,10 @@
+import acm.program.GraphicsProgram;
 
-public class pirveliAmocana {
+public class pirveliAmocana extends GraphicsProgram{
+	private static final int BRICK_WIDHT = 30;
+	private static final int BRICK_HEIGHT = 12;
+	private static final int BRICKS_IN_BASE= 14;
 
+	public void run() {
+	}
 }
