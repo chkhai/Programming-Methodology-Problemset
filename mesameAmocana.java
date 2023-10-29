45d0ba429817dba2888c49ac3d253cb28197dd3e
S <s>
2023-10-29 20:14:04 +0400

    {"types":"C","runs":22}

--- mesameAmocana.java
+++ mesameAmocana.java
@@ -4,6 +4,7 @@ import acm.program.GraphicsProgram;
 public class mesameAmocana extends GraphicsProgram{
 	private static final int RECT_HEIGHT = 80;
 	private static final int RECT_WIDTH = 200;
+	private static final int DIFFERENCE_BETWEEN_RECTS = 30;
 
 	public void run() {
 		drawConsoleProgramRectAndLabel();
@@ -11,8 +12,14 @@ public class mesameAmocana extends GraphicsProgram{
 	}
 
 	private void drawGraphicsProgramRectAndLabel() {
-		
-		
+		double x = getWidth() / 2 - 3 * RECT_HEIGHT / 2 - DIFFERENCE_BETWEEN_RECTS;
+		double y = getHeight() / 2;
+		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+		add(rect);
+		GLabel label = new GLabel("GraphicsProgram");
+		double m = x + RECT_WIDTH / 2 - label.getWidth() / 2;
+		double n = y + RECT_HEIGHT / 2 + label.getAscent() / 2;
+		add(label, m, n);
 	}
 
 	private void drawConsoleProgramRectAndLabel() {
