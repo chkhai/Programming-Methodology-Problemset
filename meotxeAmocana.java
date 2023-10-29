e059b6160e6f9d464650eef8e3a1674fc5906257
S <s>
2023-10-30 00:37:57 +0400

    {"types":"C","runs":3}

--- meotxeAmocana.java
+++ meotxeAmocana.java
@@ -1,11 +1,11 @@
 import acm.program.ConsoleProgram;
 
-public class meotxeAmocana extends ConsoleProgram{
-	
+public class meotxeAmocana extends ConsoleProgram {
+
 	public void run() {
 		int a = readInt("Enter integer: ");
 		int b = readInt("Enter integer: ");
 		double y = Math.sqrt(a * a + b * b);
 		println(y);
-	}	
+	}
 }
