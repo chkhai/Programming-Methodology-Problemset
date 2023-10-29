1ab3d73c15b56bf175dd2bb5528bd919edc9a4c0
S <s>
2023-10-29 12:09:26 +0400

    {"types":"C","runs":13}

--- meekvseAmocana.java
+++ meekvseAmocana.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class meekvseAmocana {
-
+public class meekvseAmocana extends ConsoleProgram{
+	
+	public void run() {
+		int n = readInt("Enter integer: ");
+		while(true){
+			if(n % 2 ==0){
+				n = n / 2;
+			}else{
+				n = 3 * n + 1;
+			}
+			if(n == 1){
+				println(n);
+			}
+		}
+	}
 }
