49f261f12d02cb55671997d72fec73cfef3e7c2e
S <s>
2023-10-29 12:02:31 +0400

    {"types":"C","runs":11}

--- mexuteAmocana.java
+++ mexuteAmocana.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class mexuteAmocana {
-
+public class mexuteAmocana extends ConsoleProgram{
+	private static final int q = 0;
+	public void run() {
+		while(true){
+			int k = readInt();
+			if(k == q){
+				break;
+			}
+		}
+		
+	}
 }
