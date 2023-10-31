437cae34dca3338ce0e6fde5f6cd077171afd34d
S <s>
2023-10-31 21:08:38 +0400

    {"types":"C","runs":33}

--- pirveliAmocana.java
+++ pirveliAmocana.java
@@ -17,7 +17,7 @@ public class pirveliAmocana extends GraphicsProgram {
 		double x = getWidth() / 2 - BRICK_WIDTH * (BRICKS_IN_BASE - i)/ 2;
 		double y = getHeight() - i * BRICK_HEIGHT;
 		for(int j = 0; j < BRICKS_IN_BASE; j++){
-			if(i % 2 = 0){
+			if(i % 2 == 0){
 				x = x + BRICK_WIDTH * j ;
 			}else{
 				x = x + BRICK_WIDTH * j + BRICK_WIDTH /2;
