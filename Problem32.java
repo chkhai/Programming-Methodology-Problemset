64e5aaa61c4af3c9749b9a6628ea2b1b9a00943a
S <s>
2023-10-31 18:01:46 +0400

    {"types":"C","runs":8}

--- Problem32.java
+++ Problem32.java
@@ -10,6 +10,9 @@ public class Problem32 extends ConsoleProgram{
 		}
 	}
 	private boolean isPrime(int num) {
+		if(num == 1 ){
+			return false;
+		}
 		for(int i = 2; i <= Math.sqrt(num); i++){
 			if(num % i == 0){
 				return false;
