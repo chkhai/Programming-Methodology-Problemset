955eb5eff6489ad58fb470e0aeea4bb660256950
S <s>
2023-10-31 20:57:27 +0400

    {"types":"C","runs":15}

--- pirveliAmocana.java
+++ pirveliAmocana.java
@@ -16,10 +16,10 @@ public class pirveliAmocana extends GraphicsProgram {
 	private void makeHorisontalRowOfBricks(int i) {
 		double x = getWidth() / 2 - BRICK_WIDTH * (BRICKS_IN_BASE - i)/ 2;
 		double y = getHeight() - i * BRICK_HEIGHT;
-	//	for(int j = 0; j<BRICKS_IN_BASE; j++){
-		//	x = 
+		for(int j = 0; j<BRICKS_IN_BASE; j++){
+			x = x + BRICK_WIDTH;
 			GRect rect = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
 			add(rect);
-		//}
+		}
 	}
 }
