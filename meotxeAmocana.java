1a7c73f5ccc640461c9f93e488fe08b9ee19ee75
S <s>
2023-10-31 22:16:10 +0400

    {"types":"C","runs":68}

--- meotxeAmocana.java
+++ meotxeAmocana.java
@@ -5,11 +5,11 @@ public class meotxeAmocana extends ConsoleProgram {
 	public void run() {
 		int a = readInt("Enter integer: ");
 		int b = readInt("Enter integer: ");
-		double c = calculatePithagoraemTheorem(a, b);
+		double c = calculatePythagoreanTheorem(a, b);
 		println(c);
 	}
 
-	private double calculatePithagoraemTheorem(int a, int b) {
+	private double calculatePythagoreanTheorem(int a, int b) {
 		double y = Math.sqrt(a*a +b*b);
 		return y;
 	}
