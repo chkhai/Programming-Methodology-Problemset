d634b16976ca9b94c4d0199062ad582c9f55b788
S <s>
2023-11-02 21:32:14 +0400

    {"types":"C","runs":0}

--- mexuteAmocana.java
+++ mexuteAmocana.java
@@ -5,9 +5,19 @@ public class mexuteAmocana extends ConsoleProgram{
 	public void run() {
 		println("This program finds smallest and largest numbers. ");
 		int k = readInt("?");
+		int max = k;
+		int min = k;
 		while(k != SENTINEL){
-			
+			int m = readInt("?");
+			if(m > max){
+				max = m; 
+			}	
+			if(m < min){
+				min = m;
+			}
 		}
+		println(max);
+		println(min)
 	}
 }
 
