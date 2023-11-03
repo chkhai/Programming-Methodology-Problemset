db6e8923405dd5e490d1b079c1d14c0930058e8b
S <s>
2023-11-03 21:57:58 +0400

    {"types":"C","runs":19}

--- test.java
+++ test.java
@@ -1,4 +1,16 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class test {
-
+public class test extends GraphicsProgram{
+	private static final int width = 30;
+	private static final int height = 40;
+	public void run() {
+		int x = 100;
+		int y = 200;
+		createCircle(x, y, width, height);
+	}
+	private void createCircle(int x, int y, int width2, int height2) {
+		GOval oval = new GOval(x, y , width, height);
+		add(oval);
+	}
 }
