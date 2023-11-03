e6ed22042f7e4525c23331cdf5182504f51ab261
S <s>
2023-11-03 22:07:13 +0400

    {"types":"C","runs":23}

--- meoreAmocana.java
+++ meoreAmocana.java
@@ -9,8 +9,43 @@ public class meoreAmocana extends GraphicsProgram {
 	private static final double SMALL_RADIUS = BIG_RADIUS * 76 / 254;
 
 	public void run() {
-		double x1= getWidth() - BIG_RADIUS;
-		double y1 = getHeight() - BIG_RADIUS;
-		createCircle(x1, y1, 2*BIG_RADIUS, 2*BIG_RADIUS);
+		drawFirstCircle();
+		drawSecondCircle();
+		drawThirdCircle();
+	}
+	
+	/* This method creates the smallest circle, fills it and colors it red.
+	 */
+	private void drawThirdCircle() {
+		double x = getWidth() / 2 - SMALL_RADIUS;
+		double y = getHeight() / 2 - SMALL_RADIUS;
+		GOval thirdCircle = new GOval(x, y, 2 * SMALL_RADIUS, 2 * SMALL_RADIUS);
+		thirdCircle.setFilled(true);
+		thirdCircle.setColor(Color.RED);
+		add(thirdCircle);
+	}
+	
+	/*
+	 */
+	private void drawSecondCircle() {
+		double x = getWidth() / 2 - MIDDLE_RADIUS;
+		double y = getHeight() / 2 - MIDDLE_RADIUS;
+		GOval secondCircle = new GOval(x, y, 2 * MIDDLE_RADIUS, 2 * MIDDLE_RADIUS);
+		secondCircle.setFilled(true);
+		secondCircle.setColor(Color.white);
+		add(secondCircle);
+	}
+
+	/*
+	 * 
+	 */
+	private void drawFirstCircle() {
+		double x = getWidth() / 2 - BIG_RADIUS;
+		double y = getHeight() / 2 -BIG_RADIUS;
+		GOval firstCircle = new GOval(getWidth() / 2 - BIG_RADIUS, getHeight() / 2 - BIG_RADIUS, 2 * BIG_RADIUS,
+				2 * BIG_RADIUS);
+		firstCircle.setFilled(true);
+		firstCircle.setColor(Color.RED);
+		add(firstCircle);
 	}
 }
