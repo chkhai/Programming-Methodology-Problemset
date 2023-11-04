83293f978646229c7ae1bc7be8254a2ad5836bc6
S <s>
2023-11-04 13:21:49 +0400

    {"types":"C","runs":12}

--- mexuteAmocana.java
+++ mexuteAmocana.java
@@ -9,20 +9,15 @@ public class mexuteAmocana extends ConsoleProgram {
 		int k = readInt("? ");
 		int max = k;
 		int min = k;
-		while (k != SENTINEL) {
-			int m = readInt("? ");
-			if (m == SENTINEL) {
+		while(true){
+			int n = readInt("? ");
+			if (n == SENTINEL){
 				break;
 			}
-			if (m > max) {
-				max = m;
-			}
-			if (m < min) {
-				min = m;
-			}
+			max = compareMaxAndN(max, n);
+			min = compareMinAndN(min, n);
 		}
 		println("largest: " + max);
 		println("smallest: " + min);
 	}
-
 }
