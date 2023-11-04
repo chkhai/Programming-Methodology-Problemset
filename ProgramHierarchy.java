3c194c432e0d8941d7cc6b0aa9f7f393e9459601
S <s>
2023-11-04 14:22:11 +0400

    {"types":"C","runs":1}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -1,3 +1,10 @@
+/*
+ * File: ProgramHierarchy.java
+ * Name: 
+ * Section Leader: 
+ * ---------------------------
+ * This file is the starter file for the ProgramHierarchy problem.
+ */
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
