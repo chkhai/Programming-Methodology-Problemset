574c695831206bdefca55bc51326bbb7268403c5
S <s>
2023-11-04 15:38:23 +0400

    {"types":"C","runs":9}

--- Problem33.java
+++ Problem33.java
@@ -1,4 +1,5 @@
 
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
@@ -8,6 +9,14 @@ public class Problem33 extends GraphicsProgram{
 	private static final int MIN_RADIUS = 50;
 
 	public void run(){
-		
+		addRandomCircle();
+	}
+
+	private void addRandomCircle() {
+		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
+		double y = rgen.nextDouble(0, getHeight() - 2* radius);
+		GOval oval = new GOval(2*radius, 2*radius);
+		add(oval, x, y);
 	}
 }
