1090e7002a5f4234f3bfdf0d793c06f60a9097d0
S <s>
2023-11-04 20:58:30 +0400

    {"types":"C","runs":13}

--- Problem38.java
+++ Problem38.java
@@ -7,28 +7,42 @@ import acm.util.RandomGenerator;
 უორმაგედბა დადებული თანხა წაგების შემთხვევაში კი მოთამაშე კარგავს დადებულ თანხას.
 მოთამაშე მოგებულია მაშინ თუ რულეტკაზე ამოსული რიცხვი ემთხვევა მის მიერ დადებულ
 რიცხვს. პროგრამა ყოველ მოქმედებაზე უნდა ბეჭდავდეს შესაბამის შეტყობინებას, ადვილად
-
 რომ გაერკვეს მოთამაშე, რა რიცხვი ამოვიდა, რამდენი წააგო, რამდენი მოიგო, რამდენი აქვს
 ბალანსი.
 */
-public class Problem38 extends ConsoleProgram{
+public class Problem38 extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
+
 	public void run() {
-		int money =1000;
-		while(money > 0){
+		int money = 1000;
+		while (money > 0) {
 			playTheGame(money);
 		}
 	}
 
 	private void playTheGame(int money) {
 		int number = readInt("Enter number between 0 and 36");
-		if(number>36 || number<0){
+		while (number > 36 || number < 0) {
 			println("Number must be between 0 and 36");
 			int secondNumber = readInt("Enter number between 0 and 36");
-			number =secondNumber;
+			number = secondNumber;
 		}
-		
-		
+		int betMoney = readInt("How much would you like to bet?");
+		while (betMoney > money) {
+			println("You don't have that much money");
+			int secondBetMoney = readInt("How much would you like to bet?");
+			betMoney = secondBetMoney;
+		}
+		int luckyNumber = rgen.nextInt(0, 36);
+		if (luckyNumber == number) {
+			println("Congrats, you win. The lucky number was " + luckyNumber + " and you guessed it!");
+			money = money + betMoney;
+			println("Your current money is " + money);
+		} else {
+			println("Unfortunately, You lost. The lucky number was" + luckyNumber + " and you picked " + number);
+			money = money - betMoney;
+			println("Your current money is " + money);
+		}
+
 	}
 }
