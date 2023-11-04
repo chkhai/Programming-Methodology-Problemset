87f8071e9771deb2b5874f1f8704a500e6829c1e
S <s>
2023-11-04 20:58:49 +0400

    {"types":"C","runs":14}

--- Problem38.java
+++ Problem38.java
@@ -21,7 +21,7 @@ public class Problem38 extends ConsoleProgram {
 	}
 
 	private void playTheGame(int money) {
-		int number = readInt("Enter number between 0 and 36");
+		int number = readInt("Enter number between 0 and 36: ");
 		while (number > 36 || number < 0) {
 			println("Number must be between 0 and 36");
 			int secondNumber = readInt("Enter number between 0 and 36");
