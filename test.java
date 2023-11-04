679b11631236c5ce5f62c2bfdc2c346b13cca527
S <s>
2023-11-04 15:14:46 +0400

    {"types":"C","runs":4}

--- test.java
+++ test.java
@@ -5,102 +5,36 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class test extends GraphicsProgram{
-	private static final int RECT_HEIGHT = 80;
-	private static final int RECT_WIDTH = 200;
-	private static final int WIDTH_DIFFERENCE_BETWEEN_RECTS = 20;
-	private static final int HEIGHT_DIFFERENCE_BETWEEN_RECTS = 50;
+	/** Width of each bricks in pixels */
+	private static final int BRICK_WIDTH = 30;
+
+	/** Width of each bricks in pixels */
+	private static final int BRICK_HEIGHT = 12;
+
+	/** Number of each bricks in the base of the pyramid */
+	private static final int BRICKS_IN_BASE = 14;
 
 	public void run() {
-		drawConsoleProgramRectAndLabel();
-		drawGraphicsProgramRectAndLabel();
-		drawDialogProgramRectAndLabel();
-		drawProgramRectAndLabel();
-		drawLines();
+		for (int i = 1; i < BRICKS_IN_BASE + 1; i++) {
+			makeHorizontalRowOfBricks(i);
+		}
 	}
 
 	/*
-	 * This method draws lines between rectangles. line1 is between "Program"
-	 * and "GraphicsProgram". line2 is between "Program" and "ConsoleProgram".
-	 * line3 is between "Program" and "DialogProgram".
+	 * This method helps us build horizontal row of bricks. x and y are the
+	 * coordinates of the first brick in the row. After creating the first
+	 * brick, x coordinate increases by BRICK_WIDTH, y coordinate stays the same
+	 * since its the same row. In one row we have to build BRICKS_IN_BASE-i+1
+	 * bricks.
 	 */
-	private void drawLines() {
-		double x1 = getWidth() / 2;
-		double y1 = getHeight() / 2 - HEIGHT_DIFFERENCE_BETWEEN_RECTS;
-		double x2 = getWidth() / 2 - RECT_WIDTH - WIDTH_DIFFERENCE_BETWEEN_RECTS;
-		double y2 = getHeight() / 2;
-		double x3 = getWidth() / 2 + RECT_WIDTH + WIDTH_DIFFERENCE_BETWEEN_RECTS;
-		double y3 = y2;
-		GLine line1 = new GLine(x1, y1, x2, y2);
-		GLine line2 = new GLine(x1, y1, getWidth() / 2, getHeight() / 2);
-		GLine line3 = new GLine(x1, y1, x3, y3);
-		add(line1);
-		add(line2);
-		add(line3);
-	}
-
-	/*
-	 * This method helps us draw "Program" rectangle. x and y are coordinates of
-	 * the rectangle. After drawing rectangle, method draws label in the center
-	 * of the rectangle.
-	 */
-	private void drawProgramRectAndLabel() {
-		double x = getWidth() / 2 - RECT_WIDTH / 2;
-		double y = getHeight() / 2 - HEIGHT_DIFFERENCE_BETWEEN_RECTS - RECT_HEIGHT;
-		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
-		add(rect);
-		GLabel label = new GLabel("Program");
-		double m = x + RECT_WIDTH / 2 - label.getWidth() / 2;
-		double n = y + RECT_HEIGHT / 2 + label.getAscent() / 2;
-		add(label, m, n);
-	}
-
-	/*
-	 * This method helps us draw "DialogProgram" rectangle. x and y are
-	 * coordinates of the rectangle. After drawing rectangle, method draws label
-	 * in the center of the rectangle.
-	 */
-	private void drawDialogProgramRectAndLabel() {
-		double x = getWidth() / 2 + RECT_WIDTH / 2 + WIDTH_DIFFERENCE_BETWEEN_RECTS;
-		double y = getHeight() / 2;
-		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
-		add(rect);
-		GLabel label = new GLabel("DialogProgram");
-		double m = x + RECT_WIDTH / 2 - label.getWidth() / 2;
-		double n = y + RECT_HEIGHT / 2 + label.getAscent() / 2;
-		add(label, m, n);
-
-	}
-
-	/*
-	 * This method helps us draw "GraphicsProgram" rectangle. x and y are
-	 * coordinates of the rectangle. After drawing rectangle, method draws label
-	 * in the center of the rectangle.
-	 */
-	private void drawGraphicsProgramRectAndLabel() {
-		double x = getWidth() / 2 - 3 * RECT_WIDTH / 2 - WIDTH_DIFFERENCE_BETWEEN_RECTS;
-		double y = getHeight() / 2;
-		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
-		add(rect);
-		GLabel label = new GLabel("GraphicsProgram");
-		double m = x + RECT_WIDTH / 2 - label.getWidth() / 2;
-		double n = y + RECT_HEIGHT / 2 + label.getAscent() / 2;
-		add(label, m, n);
-	}
-
-	/*
-	 * This method helps us draw "ConsoleProgram" rectangle. x and y are
-	 * coordinates of the rectangle. After drawing rectangle, method draws label
-	 * in the center of the rectangle.
-	 */
-	private void drawConsoleProgramRectAndLabel() {
-		double x = getWidth() / 2 - RECT_WIDTH / 2;
-		double y = getHeight() / 2;
-		GRect rect = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
-		add(rect);
-		GLabel label = new GLabel("ConsoleProgram");
-		double m = x + RECT_WIDTH / 2 - label.getWidth() / 2;
-		double n = y + RECT_HEIGHT / 2 + label.getAscent() / 2;
-		add(label, m, n);
+	private void makeHorizontalRowOfBricks(int i) {
+		double x = getWidth() / 2 - BRICK_WIDTH * (BRICKS_IN_BASE - i) / 2;
+		double y = getHeight() - i * BRICK_HEIGHT;
+		for (int j = 0; j < BRICKS_IN_BASE + 1 - i; j++) {
+			GRect rect = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+			add(rect);
+			x = x + BRICK_WIDTH;
+		}
 	}
 }
 	
