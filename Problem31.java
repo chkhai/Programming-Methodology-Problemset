19abfe03e03ff0ccb829610f922a8a36bc237d73
S <s>
2023-11-04 15:31:00 +0400

    {"types":"C","runs":8}

--- Problem31.java
+++ Problem31.java
@@ -6,8 +6,11 @@ public class Problem31 extends ConsoleProgram{
 		double n = readInt("Enter integer: ");
 		double count = 0;
 		if(isPrime(n,count)){
-			}
+			println(n + " is prime.");
+		}else{
+			println(n + " is not prime");
 		}
+	}
 
 	private boolean isPrime(double n, double count) {
 		for(int i = 1; i < n; i++){
