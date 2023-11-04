61bb133ed131698312d20a9331576afa5d31ca9c
S <s>
2023-11-04 12:55:02 +0400

    {"types":"C","runs":5}

--- mexuteAmocana.java
+++ mexuteAmocana.java
@@ -1,25 +1,27 @@
 import acm.program.ConsoleProgram;
+
 //not finished
-public class mexuteAmocana extends ConsoleProgram{
+public class mexuteAmocana extends ConsoleProgram {
 	private static final int SENTINEL = 0;
+
 	public void run() {
 		println("This program finds smallest and largest numbers. ");
 		int k = readInt("? ");
 		int max = k;
 		int min = k;
-		while(k != SENTINEL){
+		while (k != SENTINEL) {
 			int m = readInt("? ");
-			if(m == SENTINEL){
+			if (m == SENTINEL) {
 				break;
 			}
-			if(m > max){
-				max = m; 
-			}	
-			if(m < min){
+			if (m > max) {
+				max = m;
+			}
+			if (m < min) {
 				min = m;
 			}
-		} 
-		
+		}
+
 		println("largest: " + max);
 		println("smallest: " + min);
 	}
