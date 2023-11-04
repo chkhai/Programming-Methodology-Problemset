2e2e10623f8521908b48fc4c23b9901a8d8615a3
S <s>
2023-11-04 14:50:12 +0400

    {"types":"C","runs":2}

--- Pyramid.java
+++ Pyramid.java
@@ -1,7 +1,8 @@
+
 /*
  * File: Pyramid.java
- * Name: 
- * Section Leader: 
+ * Name: Luka Chkhaidze
+ * Section Leader: Giorgi Tkeshelashvili
  * ------------------
  * This file is the starter file for the Pyramid problem.
  * It includes definitions of the constants that match the
@@ -13,19 +14,30 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
-
 public class Pyramid extends GraphicsProgram {
+
+	/** Width of each bricks in pixels */
 	private static final int BRICK_WIDTH = 30;
+
+	/** Width of each bricks in pixels */
 	private static final int BRICK_HEIGHT = 12;
+
+	/** Number of each bricks in the base of the pyramid */
 	private static final int BRICKS_IN_BASE = 14;
 
 	public void run() {
 		for (int i = 1; i < BRICKS_IN_BASE + 1; i++) {
-			makeHorisontalRowOfBricks(i);
+			makeHorizontalRowOfBricks(i);
 		}
 	}
 
-	private void makeHorisontalRowOfBricks(int i) {
+	/*
+	 * This method helps us build horizontal row of bricks. x and y are the coordinates
+	 * of the first brick in the row. After creating the first brick, x coordinate increases by
+	 * BRICK_WIDTH, y coordinate stays the same since its the same row. In one row we have to 
+	 * build BRICKS_IN_BASE-i+1 bricks.
+	 */
+	private void makeHorizontalRowOfBricks(int i) {
 		double x = getWidth() / 2 - BRICK_WIDTH * (BRICKS_IN_BASE - i) / 2;
 		double y = getHeight() - i * BRICK_HEIGHT;
 		for (int j = 0; j < BRICKS_IN_BASE + 1 - i; j++) {
