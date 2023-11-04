49aaccd1f030ba9e7c96d5155bc0ff47bc0c6f9a
S <s>
2023-11-04 20:47:58 +0400

    {"types":"C","runs":8}

--- Problem38.java
+++ Problem38.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem38 {
+/*მოხმარებელს აქვს საწყისი თანხა 1000, თამაშობს მანამ სანამ არ წააგებს ყველაფერს. ყოველ
+ჯერზე შეყავს ფსონის თანხა და რიცხვი რომელზეც დებს. ხდება რულეტკის დატრიალების
+სიმულაცია(0-დან 36-მდე შემთხვევითი რიცხვი), მოგების შემთხვევაში მოთამაშეს
+უორმაგედბა დადებული თანხა წაგების შემთხვევაში კი მოთამაშე კარგავს დადებულ თანხას.
+მოთამაშე მოგებულია მაშინ თუ რულეტკაზე ამოსული რიცხვი ემთხვევა მის მიერ დადებულ
+რიცხვს. პროგრამა ყოველ მოქმედებაზე უნდა ბეჭდავდეს შესაბამის შეტყობინებას, ადვილად
 
+რომ გაერკვეს მოთამაშე, რა რიცხვი ამოვიდა, რამდენი წააგო, რამდენი მოიგო, რამდენი აქვს
+ბალანსი.
+*/
+public class Problem38 extends ConsoleProgram{
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run() {
+		int money =1000;
+		while(money > 0){
+			playTheGame(money);
+		}
+	}
+
+	private void playTheGame(int money) {
+		int number = readInt("Enter number between 0 and 36");
+		if(number>36 || number<0){
+			println("Number must be between 0 and 36");
+			int secondNumber = readInt("Enter number between 0 and 36");
+			secondNumber = number;
+		}
+		
+	}
 }
