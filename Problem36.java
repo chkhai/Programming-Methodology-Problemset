ff5b6a00c5aac4e67405ae8133130fb72ac02aa6
S <s>
2023-11-04 20:26:25 +0400

    {"types":"C","runs":0}

--- Problem36.java
+++ Problem36.java
@@ -6,8 +6,29 @@ public class Problem36 extends ConsoleProgram{
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int NUM_EXPERIMENTS = 10000;
 	public void run() {
+		double sumFlips=0;
 		for(int i = 0; i < NUM_EXPERIMENTS; i++) {
-			
+			int currFlips = holdExperiment();
+			sumFlips = sumFlips + currFlips;
+		} 
+		double avgFlips = sumFlips / NUM_EXPERIMENTS;
+		println(avgFlips);
+	}
+	private int holdExperiment() {
+		int numFlips = 0;
+		int threeInRow = 0;
+		while(true){
+			numFlips++;
+			boolean isHeads = rgen.nextBoolean();
+			if(isHeads){
+				threeInRow++;
+				if(threeInRow == 3){
+					break;
+				}
+			}else{
+				threeInRow=0;
+			}
 		}
+		return numFlips;
 	}
 }
