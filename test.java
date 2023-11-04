b199df50d84f22121d90363f8dc9998aa1537b1e
S <s>
2023-11-04 15:07:54 +0400

    {"types":"C","runs":1}

--- test.java
+++ test.java
@@ -1,47 +1,6 @@
 import acm.program.ConsoleProgram;
 
 public class test extends ConsoleProgram{
-	private static final int SENTINEL = 0;
-
-	public void run() {
-		println("This program finds smallest and largest numbers. ");
-		int firstInteger = readInt("? ");
-		if(firstInteger == SENTINEL){
-			println("The first number can't be " + SENTINEL);
-		}
-		int max = firstInteger;
-		int min = firstInteger;
-		while(true){
-			int otherInteger = readInt("? ");
-			if (otherInteger == SENTINEL){
-				break;
-			}
-			max = compareMaxAndN(max, otherInteger);
-			min = compareMinAndN(min, otherInteger);
-		}
-		println("largest: " + max);
-		println("smallest: " + min);
-	}
-
-	/*
-	 * This method helps us compare two integers. After comparing them, it keeps
-	 * the track of the smallest one.
-	 */
-	private int compareMinAndN(int min, int otherInteger) {
-		if (otherInteger < min) {
-			min = otherInteger;
-		}
-		return min;
-	}
-
-	/*
-	 * This method helps us compare two integers. After comparing them, it keeps
-	 * the track of the biggest one.
-	 */
-	private int compareMaxAndN(int max, int otherInteger) {
-		if (otherInteger > max) {
-			max = otherInteger;
-		}
-		return max;
-	}
+	
 }
+	
