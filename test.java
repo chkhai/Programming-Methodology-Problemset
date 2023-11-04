10a6098fa4f370ddf0af31f239bb5daf1a3cd26e
S <s>
2023-11-04 15:11:25 +0400

    {"types":"C","runs":0}

--- test.java
+++ test.java
@@ -1,6 +1,35 @@
 import acm.program.ConsoleProgram;
 
 public class test extends ConsoleProgram{
-	
+	public void run() {
+		int n = readInt("Enter integer: ");
+		int count = 0;
+		count = FromNtoOne(n, count);
+		print("The process took " + count + " to reach 1.");
+	}
+
+	/*
+	 * This method helps us to reach one from n. If n is odd, we multiply it by
+	 * three and add one. The new n is 3*n+1. If n is even, we divide it by two
+	 * and new n is n/2. In both cases count increases by one. When n becomes
+	 * one, the method stops.
+	 * 
+	 */
+	private int FromNtoOne(int n, int count) {
+		while (true) {
+			if (n % 2 == 0) {
+				println(n + " is even so I take half: " + n / 2);
+				n = n / 2;
+				count++;
+			} else {
+				println(n + " is odd, so I make 3n+1:" + (3 * n + 1));
+				n = 3 * n + 1;
+				count++;
+			}
+			if (n == 1) {
+				return count;
+			}
+		}
+	}
 }
 	
