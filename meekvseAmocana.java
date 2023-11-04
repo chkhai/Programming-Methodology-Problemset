2cc18356bf1a0aaa1ecb90e50d916c390ecb3107
S <s>
2023-11-04 13:02:58 +0400

    {"types":"C","runs":7}

--- meekvseAmocana.java
+++ meekvseAmocana.java
@@ -5,20 +5,6 @@ public class meekvseAmocana extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter integer: ");
 		int count = 0;
-/*		while (true) {
-			if (n % 2 == 0) {
-				println(n + " is even so I take half: " + n / 2);
-				n = n / 2;
-				count++;
-			} else {
-				println(n + " is odd, so I make 3n+1:" + (3 * n + 1));
-				n = 3 * n + 1;
-				count++;
-			}
-			if (n == 1) {
-				break;
-			}
-		} */
 		count = FromNtoOne(n, count);
 		print("The process took " + count + " to reach 1.");
 	}
