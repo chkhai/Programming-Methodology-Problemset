25265f38f27184aa7ab5beff8c9fff01ac207975
S <s>
2023-11-07 16:55:58 +0400

    {"types":"C","runs":8}

--- Problem41.java
+++ Problem41.java
@@ -1,4 +1,19 @@
+//not finished
+import java.awt.Color;
 
-public class Problem41 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Problem41 extends GraphicsProgram{
+	private static final int RADIUS = 25;
+
+	public void run() {
+		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		circle.setColor(Color.pink);
+		while(true){
+			waitForClick();
+			double x = 
+		}
+	}
 }
