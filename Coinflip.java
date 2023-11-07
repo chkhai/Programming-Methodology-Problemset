5e97b68ad400662da1d0d79519a8083699bd87ae
S <s>
2023-11-07 17:43:57 +0400

    {"types":"C","runs":15}

--- Coinflip.java
+++ Coinflip.java
@@ -9,16 +9,20 @@ import acm.util.RandomGenerator;
 public class Coinflip extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int PAUSE_TIME = 10;
+
 	public void run() {
 		drawCircle(Color.red);
 		drawCircle(Color.green);
+		waitForClick();
 		
+
 	}
+
 	private void drawCircle(Color color) {
 		GOval circle = new GOval(getWidth() / 2 - 100, getHeight() / 2 - 100, 200, 200);
 		circle.setFilled(true);
 		circle.setColor(color);
 		add(circle);
 	}
-	
+
 }
