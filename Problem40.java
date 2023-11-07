b8acdde45f554fea3d4f60628a6bd5e4c7cf0d4a
S <s>
2023-11-07 15:06:48 +0400

    {"types":"C","runs":6}

--- Problem40.java
+++ Problem40.java
@@ -1,4 +1,19 @@
+import java.awt.Color;
 
-public class Problem40 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Problem40 extends GraphicsProgram{
+	private static final int RADIUS = 25;
+
+	public void run() {
+		GOval circle = new GOval(0, getHeight() / 2 - RADIUS, 2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		circle.setColor(Color.red);
+		add(circle);
+		while(circle.getX() != getWidth() - 2 * RADIUS){
+			circle.move(1, 0);
+			pause(5);
+		}
+	}
 }
