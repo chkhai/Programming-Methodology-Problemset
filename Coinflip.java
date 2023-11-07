493f9556c51efd97374b622afec80651bc596cd6
S <s>
2023-11-07 17:28:17 +0400

    {"types":"C","runs":12}

--- Coinflip.java
+++ Coinflip.java
@@ -1,4 +1,12 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+//გრაფიკულ პროგრამაში გააკეთეთ მონეტის აგდების სიმულაციის თამაში.
+//მონეტა არის წრე, რომელსაც ერთი მხარე აქვს წითელი და აწერია heads, მეორე მხარე აქვს მწვანე და აწერია tails.
 
-public class Coinflip {
-
+public class Coinflip extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int PAUSE_TIME = 10;
+	public void run() {
+		
+	}
 }
