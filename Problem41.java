3330bf46c62113be606e5b22ed3ab47004838e8c
S <s>
2023-11-09 17:27:32 +0400

    {"types":"C","runs":1}

--- Problem41.java
+++ Problem41.java
@@ -1,5 +1,6 @@
 //not finished
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
@@ -7,11 +8,18 @@ import acm.program.GraphicsProgram;
 //ეკრანზე დაამატეთ გაფერადებული წრეწირები იმავე წერტილში სადაც მაუსი მდებარეობს.
 public class Problem41 extends GraphicsProgram{
 	private static final int RADIUS = 25;
-
+	private GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
 	public void run() {
 		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
 		circle.setFilled(true);
 		circle.setColor(Color.pink);
 		addMouseListeners();
 	}
+	
+	public void mouseMoved(MouseEvent e){
+		add(circle, e.getX(), e.getY());
+	}
+	
 }
+
+
