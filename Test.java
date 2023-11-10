05a54ea8334794971f75260e4c05b3098afb6f10
S <s>
2023-11-10 23:30:50 +0400

    {"types":"C","runs":11}

--- Test.java
+++ Test.java
@@ -86,7 +86,7 @@ public class Test extends GraphicsProgram {
 		if(rgen.nextBoolean()){
 			vx=-vx;
 		}
-		while(BALL.getX() <= getWidth() && BALL.getY() <= getHeight()){
+		while(BALL.getX() <= getWidth() - 2 * BALL_RADIUS && BALL.getY() <= getHeight() - 2 * BALL_RADIUS){
 			BALL.move(vx, vy);
 			pause(PAUSE_TIME);
 		}
