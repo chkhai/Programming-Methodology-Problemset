00ea733f9d5ed6c4dad79bfd65ea2f6f2da7ccbe
S <s>
2023-11-16 17:33:44 +0400

    {"types":"C","runs":20}

--- Problem46.java
+++ Problem46.java
@@ -2,8 +2,11 @@ import acm.program.ConsoleProgram;
 //დაწერეთ მეთოდი რომელსაც გადაეცემა სტიგნი და რომელიც აბრუნებს ამ სტრინგში
 //ყველაზე ხშირ სიმბოლოს.
 public class Problem46 extends ConsoleProgram{
+	
 	public void run(){
 		String word = readLine("Enter word: ");
-		
+		for(int i = 0; i < word.length(); i++){
+			
+		}
 	}
 }
