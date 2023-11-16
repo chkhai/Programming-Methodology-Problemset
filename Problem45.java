6102497fe6c9d7c9b7e1039f09dd5ce67b2e2448
S <s>
2023-11-16 17:21:49 +0400

    {"types":"C","runs":16}

--- Problem45.java
+++ Problem45.java
@@ -1,9 +1,10 @@
 import acm.program.ConsoleProgram;
-
+//მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
+//რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
 public class Problem45 extends ConsoleProgram{
 	public void run() {
-		String str1 = "mari";
-		String str2 = "mari";
-		String str3 = new String ("mari");
+		String word = readLine("Enter word: ");
+		String symb = readLine("Enter symbol: ");
+		
 	}
 }
