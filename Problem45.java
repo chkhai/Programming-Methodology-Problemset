dc7b79630f697fd92bbe1f9d631b8b64a18b0e4b
S <s>
2023-11-16 17:27:31 +0400

    {"types":"C","runs":16}

--- Problem45.java
+++ Problem45.java
@@ -5,6 +5,12 @@ public class Problem45 extends ConsoleProgram{
 	public void run() {
 		String word = readLine("Enter word: ");
 		String symb = readLine("Enter symbol: ");
-		
+		int count = 0;
+		for(int i = 0; i < word.length(); i++){
+			if(word.charAt(i)==symb.charAt(1)){
+				count++;
+			}
+		}
+		println(count);
 	}
 }
