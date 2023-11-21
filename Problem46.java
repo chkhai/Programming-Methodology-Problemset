08b60ca88dee18b226e517052524ad8f4e575e28
S <s>
2023-11-21 17:22:31 +0400

    {"types":"C","runs":7}

--- Problem46.java
+++ Problem46.java
@@ -15,7 +15,7 @@ public class Problem46 extends ConsoleProgram{
 
 	private boolean isPalindrom(String word) {
 		for(int i = 0; i < word.length(); i++){
-			if(word.charAt(i)!= word.charAt(word.length() - 1)){
+			if(word.charAt(i) != word.charAt(word.length() - i)){
 				return false;
 			}
 		}
