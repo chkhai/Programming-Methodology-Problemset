c67f5246f5c361fd15383cbbcd41730bdb42b890
S <s>
2023-11-22 19:26:24 +0400

    {"types":"C","runs":23}

--- Problem48.java
+++ Problem48.java
@@ -1,8 +1,13 @@
 import acm.program.ConsoleProgram;
-
+//კონსოლიდან შეგყვავს ტექსტი პროგრამამ უნდა დაბეჭდოს სტატისტიკა რომელი სიმბოლო
+//რამდენჯერ გვხვდება
 public class Problem48 extends ConsoleProgram{
 	
 	public void run() {
-		
+		String input = readLine("Enter a word: ");
+		for(int i = 0; i < input.length(); i++){
+			char ch = input.charAt(i);
+			println(ch);
+		}
 	}
 }
