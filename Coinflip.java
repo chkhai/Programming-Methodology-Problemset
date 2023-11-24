6ea3e7af538b90e3cc498940a7129873ae8ac714
S <s>
2023-11-24 13:22:44 +0400

    {"types":"AR","runs":16}

---
+++ Coinflip.java
@@ -0,0 +1,19 @@
+import acm.graphics.*;
+import acm.program.ConsoleProgram;
+public class Coinflip extends ConsoleProgram{
+public void run(){
+int x = 1;
+int y = 2;
+GObject o = new GRect(x,y);
+String s = "shalaxo";
+println(iseRa(x,y,o,s));
+println(iseRa(x,y,o,s));
+}
+private int iseRa(int x, double y, GObject r, String s){
+x++;
+y += 1;
+r.move(x,y);
+s = s.substring(s.indexOf('a'));
+return (int)(x + y + r.getX() + s.length());
+}
+}
