0d90fbf1eb4f54b44929d8d05b22ae1160b66337
S <s>
2023-11-24 12:50:46 +0400

    {"types":"C","runs":0}

--- MidTerm12.java
+++ MidTerm12.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
+//დაწერეთ მეთოდი getBiggestDivisor, რომელსაც გადაეცემა ნატურალური რიცხვი n და რომელიც
+//აბრუნებს ამ რიცხვის ყველაზე დიდ მარტივ გამყოფს. ჩათვალეთ, რომ n>1
+//მაგალითად 24-ის ყველაზე დიდი მარტივი გამყოფია 3, ხოლო 37-ის ყველაზე დიდი მარტივი
+//გამყოფია 37
+public class MidTerm12 extends ConsoleProgram{
+	
+	public void run() {
+		int a = readInt("Enter natural number: ");
+		int BiggestDivisor = 1;
+		for(int i = 0; i < a; i++){
+			if(a % i == 0 && isPrime(i)){
+				BiggestDivisor = i;
+			}
+		}
+		println("The biggest divisor of " + a + " is " + BiggestDivisor);
+	}
 
-public class MidTerm12 {
-
+	private boolean isPrime(int i) {
+		for(int j = 2; j < i; j++){
+			if(i % j == 0){
+				return false;
+			}
+		}
+		return true;
+	}
 }
