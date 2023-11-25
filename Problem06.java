fd35cdba311175ab63ce4764f57a4be1f275baaa
S <s>
2023-11-25 21:16:49 +0400

    {"types":"C","runs":21}

--- Problem06.java
+++ Problem06.java
@@ -1,6 +1,7 @@
 import stanford.karel.Karel;
+import stanford.karel.SuperKarel;
 
-public class Problem06 extends Karel {
+public class Problem06 extends SuperKarel {
 	/*
 	 * კარელმა უნდა შეკრიბოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე m
 ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n+m ბრილიანტი. ჩათვალეთ, რომ
@@ -30,11 +31,8 @@ public class Problem06 extends Karel {
 			turnAround();
 		}
 	}
-	
-	private void turnAround() {
-			turnLeft();
-			turnLeft();
-	}
 }
+	
+
 
 
