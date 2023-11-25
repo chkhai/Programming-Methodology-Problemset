74d1cfd53165b61b179559293e51da50d3a85a72
S <s>
2023-11-25 21:26:14 +0400

    {"types":"C","runs":23}

--- Problem48.java
+++ Problem48.java
@@ -9,12 +9,18 @@ public class Problem48 extends ConsoleProgram{
 	}
 
 	private void keepTheTrackOfSymbols(String input) {
-		for(int i = 0; i < input.length(); i++) {
-			char currCh = input.charAt(i);
-	//		if(charWasInSentence(currCh)){
-				
+		for(int i = 0; i < input.length() - 1; i++){
+			char chc = input.charAt(i);
+			for(int j = 0; j < input.length() - 1; j++){
+				char ch = input.charAt(j);
+				int count = 0;
+				if(ch==chc){
+					count++;
+					println(ch " is " + count + "times");
+				}
 			}
 		}
 		
 	}
+}
 
