f22cdb8059a7dfa0d6f22e83237cb24b51827815
S <s>
2023-11-26 15:37:15 +0400

    {"types":"C","runs":0}

--- LogarithmProblem.java
+++ LogarithmProblem.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class LogarithmProblem {
+public class LogarithmProblem extends ConsoleProgram{
+	
+	public void run() {
+		double m = readDouble("Enter natural number:");
+		double n = readDouble("Enter natural number:");
+		int k = log(m, n);
+		println(k);
+	}
 
+	private int log(double m, double n) {
+		
+		return 0;
+	}
 }
