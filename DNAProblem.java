cacae6411d765f143e25882289bb8cab221c171d
S <s>
2023-11-26 16:24:18 +0400

    {"types":"C","runs":33}

--- DNAProblem.java
+++ DNAProblem.java
@@ -31,6 +31,7 @@ public class DNAProblem extends ConsoleProgram {
 				System.out.println("true");
 				char chch = luke.charAt(j);
 				if (ch == chch) {
+					System.out.println("true");
 					length = checkTheLengthOfSamePart(anakin, luke, ch, chch, i, j);
 //					System.out.println("true");
 					if (length > (anakin.length() / 2)) {
