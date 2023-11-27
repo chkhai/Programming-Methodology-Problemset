25df804a42c2670c21fb760d03837507fcc7aa1e
S <s>
2023-11-28 00:29:56 +0400

    {"types":"C","runs":1}

--- SpellWord.java
+++ SpellWord.java
@@ -1,4 +1,32 @@
+import acm.program.ConsoleProgram;
 
-public class SpellWord {
-
+public class SpellWord extends ConsoleProgram{
+	
+	private String text1;
+	
+	private String text2;
+	
+	public void run(){
+		text1 = readLine("Enter text: "); 
+		text2 = readLine("Enter text to see if we can spell it: "); 
+		if(weCanSpell()){
+			println("true");
+		}else{
+			println("false");
+		}
+	}
+	
+	private boolean weCanSpell(){
+		for(int i = 0; i < text2.length(); i++){
+			int m = text1.indexOf(i);
+			if(m != -1){
+				text1= text1.substring(0,i) + text1.substring(i);
+			}
+		}
+		if(text1.length()>0){
+			return true;
+		}
+		return false;
+	}
+	
 }
