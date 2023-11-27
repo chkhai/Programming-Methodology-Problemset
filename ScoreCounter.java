bef8caa1f3c7f2eda368d97fdb45b8dd4958073f
S <s>
2023-11-28 00:12:50 +0400

    {"types":"C","runs":0}

--- ScoreCounter.java
+++ ScoreCounter.java
@@ -1,4 +1,41 @@
+import java.awt.event.MouseEvent;
 
-public class ScoreCounter {
+import acm.graphics.*;
+import acm.program.*;
 
+public class ScoreCounter extends GraphicsProgram{
+	
+	private static final int RAD = 100;
+	
+	private GRect rect;
+	
+	GLabel label;
+	
+	private int num;
+	
+	private int click1;
+	
+	public void run() {
+		rect = new GRect(getWidth() / 2 - RAD, getHeight() / 2 - RAD, 2* RAD, 2* RAD);
+		add(rect);
+		num = 0;
+		GLabel label = new GLabel(""+num);
+		add(label, getWidth() / 2 -label.getWidth(), getHeight() / 2- label.getAscent());
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e){
+		click1 = e.getX();
+	}
+	
+	public void mouseDragged(MouseEvent e){
+		if(e.getX() > getWidth()/2 - RAD && e.getX() < getWidth()/2 + RAD &&
+				e.getY() > getHeight()/2-RAD && e.getY() < getHeight()/2+RAD){
+			if(e.getX() > click1 && num!=9){
+				num++;
+			}else if(e.getX() < click1 && num!=0){
+				num--;
+			}
+		}
+	}
 }
