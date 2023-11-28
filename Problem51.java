b45af9d5a34fcef168166e39d0f058c022b5fe1e
S <s>
2023-11-28 17:21:52 +0400

    {"types":"C","runs":6}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class Problem51 {
+//დაწერეთ მეთოდი რომელიც გადაცემული ტექსტიდან შლის დუბლირებულ სიმბოლოებს,
+//ანუ თუ ტექსტიში რომელიმე სიმბოლო მეორდება, უნდა დარჩეს მხოლოდ ყველაზე მარცხენა.
 
+public class Problem51 extends ConsoleProgram{
+	
+	public void run(){
+		String str = readLine("Enter string: ");
+		str = updateStr(str);
+		println(str);
+	}
+	
+	
+	private String updateStr(String str){
+		for(int i = 0; i < str.length(); i++){
+			char ch = str.charAt(i);
+			int first = str.indexOf(ch);
+			int last = str.lastIndexOf(ch);
+			if(first != -1 && last !=-1 && last!=first){
+				str = str.substring(0,last)+str.substring(last+1);
+			}
+		}
+		return str;
+	}
+	
 }
