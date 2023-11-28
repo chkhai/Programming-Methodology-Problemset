c9dcc6d5cb1272e3eeaf931040a47bcc48c2fda2
S <s>
2023-11-28 17:55:20 +0400

    {"types":"C","runs":10}

--- MidtermPractice.java
+++ MidtermPractice.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class MidtermPractice {
+////კონსოლიდან შემოდის ტექსტი. დაბეჭდეთ True თუ ტექსტში არსებული ყველა 
+//lowercase ინგლისური ასო ზრდადობით არის დალაგებული, False თუ არაა. ტექსტში სხვა სიმბოლოებიც
+//შეიძლება შეგხვდეთ და იმათ ყურადღება არ მიაქციოთ.  მაგ: “a bcAAA!” -> true, “” -> true “ABCDz,a” -> 
+//false (Practice02)Tags: String, char, charOperations, isLowerCase   
+
+
+
+public class MidtermPractice extends ConsoleProgram{
+	
+	public void run() {
+		String str = readLine("Enter string: ");
+	}
 
 }
