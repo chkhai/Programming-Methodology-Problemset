9e9dcc2c23a477fa948ca833352251f9eb65d7fd
S <s>
2023-12-01 20:40:47 +0400

    {"types":"C","runs":66}

--- Flicker.java
+++ Flicker.java
@@ -1,4 +1,22 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-public class Flicker {
-
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+//გააკეთეთ Paint-ის ფანქრის ანალოგიური ხელსაწყო. კერძოდ, მაუსის ყოველ მოძრაობაზე
+//ეკრანზე დაამატეთ გაფერადებული წრეწირები იმავე წერტილში სადაც მაუსი მდებარეობს.
+public class Flicker extends GraphicsProgram{
+	private static final int RADIUS = 25;
+	private GOval circle;
+	public void run() {
+		circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		circle.setColor(Color.pink);
+		addMouseListeners();
+	}
+	
+	public void mouseMoved(MouseEvent e){
+		add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
+	}
+	
 }
