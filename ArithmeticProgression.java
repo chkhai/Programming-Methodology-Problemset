1c9a0e998975c84150eed021e53c15daf2c6351c
S <s>
2023-12-01 19:29:12 +0400

    {"types":"C","runs":1}

--- ArithmeticProgression.java
+++ ArithmeticProgression.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class ArithmeticProgression {
+//თქვენი ამოცანაა დაადგინოთ არის თუ არა მომხმარებლის მიერ შემოყვანილი რიცხვები
+//არითმეტიკული პროგრესია. ამისათვის თქვენ უნდა დაწეროთ კონსოლის პროგრამა.
+//ჩათვალეთ რომ მომხმარებელს მხოლოდ მთელი რიცხვები შემოჰყავს მანამ სანამ არ
+//შემოიყვანს -1. როგორც კი მომხმარებელი -1 შემოიყვანს თქვენმა პროგრამამ უნდა
+//დაბეჭდოს სტრინგი “Arithmetic progresson” თუკი შეყვანილი მიმდევრობა
+//არითმეტიკული პროგრესიია და უნდა დაბეჭდოს სტრინგი “Not an arithmetic
+//porgression” წინააღმდეგ შემთხვევაში. რის შემდეგაც პროგრამამ უნდა დაასრულოს
+//მუშაობა
 
+public class ArithmeticProgression extends ConsoleProgram{
+	private static final int SENTINEL = -1;
+	public void run(){
+		int count = 0;
+		while(true){
+			int k = readInt("Enter number: ");
+			if(k == S)
+		}
+	}
 }
