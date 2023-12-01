4bdb5f6cf961f2db6cb14521d38dd7d31b074b96
S <s>
2023-12-01 19:39:00 +0400

    {"types":"C","runs":2}

--- unEvilProblem.java
+++ unEvilProblem.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
 
-public class unEvilProblem {
+//ტექსტში, რომელშიც ერთიდაიგივე სიმბოლო გვერდიგვერდ არ გვხდებოდა, ვიღაც
+//ბოროტმა კაცმა სხვადასხვა ადგილებში ჩაამატა ერთიდაიგივე სიმბოლოები ორ-ორად,
+//ერთმანეთის გვერდით. ამ ჩამატებულ და შეცვლილ სტინგში კიდევ ჩაამატა
+//ერთმანეთის გვერდით ერთიდაიგივე სიმბოლოები და ა.შ. სანამ არ მობეზრდა.
+//“How are you?” -&gt; “How are yoxxu?” -&gt; “Hoyyw are yoxxu?” -&gt; “Hoyyw are
+//yoxzzxu?” -&gt; “Hoyyw aaare yoxzzxu?”.
+//თქვენი ამოცანაა აღადგინოთ საწყისი ტექსტი. ამისათვის უნდა დაწეროთ unEvilText()
+//მეთოდი რომელსაც გადაეცემა სტრინგი და აბრუნებს სტრინგს. გადაცემულ სტრინგში
+//წერია ბოროტი კაცის მიერ ზემოთ აღწერილი მეთოდით “გაფუჭებული” ტექსტი.
+//მეთოდმა უნდა დააბრუნოს თავდაპირველი ტექსტი.
 
+public class unEvilProblem extends ConsoleProgram{
+
+	public void run() {
+		String str = readLine("Enter string: ");
+		str = restoreStr(str);
+	}
+	
+	
+	private String restoreStr(String str){
+		for(int i = 0; i < str.length(); i++){
+			char ch = str.charAt(i);
+			char chch = str.charAt(i+1);
+			if(ch == chch){
+				str = str.substring(0, i) + str.substring(i+2);
+			}
+		}
+		return str;
+	}
+	
 }
