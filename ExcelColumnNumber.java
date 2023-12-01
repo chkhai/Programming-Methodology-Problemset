565d3d050fc4277bd235e79816a8848a42363f2c
S <s>
2023-12-02 00:59:37 +0400

    {"types":"C","runs":6}

--- ExcelColumnNumber.java
+++ ExcelColumnNumber.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
 
-public class ExcelColumnNumber {
+public class ExcelColumnNumber extends ConsoleProgram{
 
+	public void run() {
+		int columnNumber = readInt("Enter column number: ");
+		String str = detectColumnName(columnNumber);
+		println(str);
+	}
+	
+	
+	private String detectColumnName(int columnNumber){
+		String str = "";
+
+        while (columnNumber > 0) {
+            
+            int rem = (columnNumber - 1) % 26;
+
+            char digit = (char) ('A' + rem);
+
+            str = digit + str;
+
+            columnNumber = (columnNumber - 1) / 26;
+        }
+
+        return str;
+	}
+	
 }
