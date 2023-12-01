5b41def42182ecc421dcffc55cf4ca7d79cb013e
S <s>
2023-12-02 00:45:42 +0400

    {"types":"C","runs":5}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -6,15 +6,34 @@
  */
 
 import acm.program.*;
+import acm.util.RandomGenerator;
+
+//რა არის ალბათობა იმისა, რომ  6 გვერდიანი კამათლის 4-ჯერ გაგორების შედეგად ზედიზედ ამოვა 2 ცალი 2-იანი. 
+//ამისათვის გააკეთეთ კამათლისა და გაგორებების სიმულაცია. ჩაატარეთ 10000 ცალი ექსპერიმენტი და დათვალეთ ხდომილების ალბათობა.
+//ერთი ექსპერიმენტი არის კამათლის 4-ჯერ გაგორება. (Practice08)
+//Tags: RandomGenerator, Simulations/Experiments , Probability, Dice
+
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	private static final int TRIES = 1000;
+	
 	public void run() {
-//		String str = readLine("Enter string:");
-	//	int m = Integer.parseInt(str);
-		double k = readDouble("Enter double: ");
-		int o = (int)k;
-		println(o);
-	}
+		
+//		int count = 0;
+//		int avg;
+//		for(int i = 0; i < TRIES; i++){
+//			checkTheProcces(count, avg);
+//		}
+//	}
+//
+//	private void checkTheProcces(int count, int avg) {
+//		for(int i = 0; i < 4; i++){
+//			int n = rgen.nextInt(1,6);
+//		}
+//		
+//	}
 
 }
