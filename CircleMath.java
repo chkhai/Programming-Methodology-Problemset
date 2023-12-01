254fce05951d176aaf526ba6f958839de4da82b6
S <s>
2023-12-01 19:52:11 +0400

    {"types":"C","runs":7}

--- CircleMath.java
+++ CircleMath.java
@@ -1,4 +1,41 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class CircleMath {
+//დაწერეთ გრაფიკული თამაში, რომელიც გაშვებისთანავე დაამატებს CIRCLE_NUM
+//წყვილ CIRCLE_D დიამეტრის მქონე წრეს. წყვილში ორივე წრე უნდა იყოს
+//გაფერადებული ერთი და იმავე შემთხვევითი ფერით და თითოეულისთვის უნდა
+//შეირჩეს ცალცალკე შემთხვევითი ლოკაცია.
+//ამის შემდეგ პროგრამა იგივე პირობებით ყოველ DELAY მილიწამში ერთხელ ამატებს
+//ახალ წყვილს, მანამ სანამ არ დასრულდება თამაში. თუ მომხმარებელი ერთმანეთის
+//მიმდევრობით აკლიკებს ერთსა და იმავე ფერის წრეს, მაშინ ორივე წრე ქრება.
+//გაითვალისწინეთ რომ წრეები ქრებიან მხოლოდ მაშინ როდესაც ერთმანეთის
+//მიყოლებით ორ ერთსა და იმავე ფერის წრეს ვაკლიკავთ. ანუ ამ კლიკებს შორის არ
+//უნდა მოხდეს კლიკი სხვა მესამე წრეზე ან ცარიელ ადგილზე. თამაში მთავრდება როცა
+//ყველა წრე გაქრება დაფიდან. თამაშის დამთავრებისას პროგრამაც უნდა გაჩერდეს და
+//აღარ დახატოს ახალი წრეები.
 
+public class CircleMath extends GraphicsProgram{
+	
+	private final static int CIRCLE_D = 40;
+	private final static int CIRCLE_NUM = 25;
+	private final static int DELAY = 2000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	GOval o;
+	
+	public void init() {
+		for(int i=0; i < CIRCLE_NUM; i++){
+			int x1 = rgen.nextInt();
+			int y1 = rgen.nextInt();
+			int x2 = rgen.nextInt();
+			int y2 = rgen.nextInt();
+			o = new GOval(CIRCLE_D, CIRCLE_D);
+			add(o, x1, y1);
+			add(o, x2, y2);
+		}
+	}
+	
+	public void run() {
+		
+	}
 }
