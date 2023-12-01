6b4cfa68b19bc5fa85b9c5e57d2686607fa795f0
S <s>
2023-12-01 19:55:48 +0400

    {"types":"C","runs":13}

--- CircleMath.java
+++ CircleMath.java
@@ -33,7 +33,6 @@ public class CircleMath extends GraphicsProgram{
 			o.setFilled(true);
 			o.setColor(rgen.nextColor());
 			add(o, x1, y1);
-			o.setColor(rgen.nextColor());
 			add(o, x2, y2);
 		}
 	}
