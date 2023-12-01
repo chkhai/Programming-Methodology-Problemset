13a32750bd04729113bad05a53cc8d9d778419d8
S <s>
2023-12-01 20:03:35 +0400

    {"types":"C","runs":23}

--- CircleMath.java
+++ CircleMath.java
@@ -20,31 +20,36 @@ import acm.util.RandomGenerator;
 public class CircleMath extends GraphicsProgram{
 	
 	private final static int CIRCLE_D = 40;
-	private final static int CIRCLE_NUM = 25;
+	private final static int CIRCLE_NUM = 2;
 	private final static int DELAY = 2000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	GOval o1;
-	GOval o2;
+	GOval o;
+	int x1;
+	int y1;
+	int x2;
+	int y2 ;
 	
 	public void init() {
 		for(int i=0; i < CIRCLE_NUM; i++){
-			int x1 = rgen.nextInt(0, getWidth() - CIRCLE_D);
-			int y1 = rgen.nextInt(0, getHeight() - CIRCLE_D);
-			int x2 = rgen.nextInt(0, getWidth() - CIRCLE_D);
-			int y2 = rgen.nextInt(0, getHeight() - CIRCLE_D);
-			o1 = new GOval(CIRCLE_D, CIRCLE_D);
-			o2 = new GOval(CIRCLE_D, CIRCLE_D);
-			o1.setFilled(true);
-			o2.setFilled(true);
+			x1 = rgen.nextInt(0, getWidth() - CIRCLE_D);
+			y1 = rgen.nextInt(0, getHeight() - CIRCLE_D);
+			x2 = rgen.nextInt(0, getWidth() - CIRCLE_D);
+			y2 = rgen.nextInt(0, getHeight() - CIRCLE_D);
+			o = new GOval(CIRCLE_D, CIRCLE_D);
+			o.setFilled(true);
 			Color color = rgen.nextColor();
-			o1.setColor(color);
-			add(o1, x1, y1);
-			o2.setColor(color);
-			add(o2, x2, y2);
+			o.setColor(color);
+			add(o, x1, y1);
+			o.setColor(color);
+			add(o, x2, y2);
 		}
 	}
 	
 	public void run() {
-		
+		addMouseListeners();
+		while(true){
+			
+			
+		}
 	}
 }
