0ddf22ed6b86631fd6bf15ba73a9cbf05867fbf6
S <s>
2023-12-01 20:38:58 +0400

    {"types":"C","runs":63}

--- Flicker.java
+++ Flicker.java
@@ -8,15 +8,16 @@ import javafx.scene.input.MouseEvent;
 public class Flicker extends GraphicsProgram{
 	private static final int RAD = 30;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	GObject obj;
+	GOval circle;
 	GOval oval = new GOval(2*RAD, 2*RAD);
-	public void run(){
+	public void run() {
+		circle = new GOval(2 * RAD, 2 * RAD);
+		circle.setFilled(true);
+		circle.setColor(Color.pink);
 		addMouseListeners();
 	}
 	
-	public void mouseClicked(MouseEvent e){
-		double x = e.getX();
-		double y = e.getY();
-		add(oval, x - RAD, y - RAD);
+	public void mouseMoved(MouseEvent e){
+		add(circle, e.getX() - RAD, e.getY() - RAD);
 	}
 }
