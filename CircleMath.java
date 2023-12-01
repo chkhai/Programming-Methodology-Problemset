a7c2bf77984ae23ba888fd1264e3a27e5a333eb1
S <s>
2023-12-01 20:07:56 +0400

    {"types":"C","runs":28}

--- CircleMath.java
+++ CircleMath.java
@@ -20,7 +20,7 @@ import acm.util.RandomGenerator;
 public class CircleMath extends GraphicsProgram{
 	
 	private final static int CIRCLE_D = 40;
-	private final static int CIRCLE_NUM = 1;
+	private final static int CIRCLE_NUM = 2;
 	private final static int DELAY = 2000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	GOval o;
@@ -34,21 +34,17 @@ public class CircleMath extends GraphicsProgram{
 		for(int i=0; i < CIRCLE_NUM; i++){
 			x1 = rgen.nextInt(0, getWidth() - CIRCLE_D);
 			y1 = rgen.nextInt(0, getHeight() - CIRCLE_D);
-			x2 = rgen.nextInt(0, getWidth() - CIRCLE_D);
-			y2 = rgen.nextInt(0, getHeight() - CIRCLE_D);
+//			x2 = rgen.nextInt(0, getWidth() - CIRCLE_D);
+//			y2 = rgen.nextInt(0, getHeight() - CIRCLE_D);
 			o = new GOval(CIRCLE_D, CIRCLE_D);
 			o.setFilled(true);
 			color = rgen.nextColor();
-			o.setColor(color);
-			createTwoCircle();
+			for(int j = 0; j<2; j++){
+				add(o, x1, y1);
+			}
 		}
 	}
 	
-	private void createTwoCircle() {
-		add(o, x1, y1);
-		add(o, x2, y2);
-	}
-
 	public void run() {
 		addMouseListeners();
 		while(true){
