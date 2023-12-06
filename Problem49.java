223947c28a27eaea5e4c254ede7e454f48d07940
S <s>
2023-12-06 20:51:05 +0400

    {"types":"C","runs":1}

--- Problem49.java
+++ Problem49.java
@@ -2,18 +2,19 @@ import acm.program.ConsoleProgram;
 
 public class Problem49 extends ConsoleProgram{
 	public void run(){
-//		String str = "3456";
-//		int num = stringToInteger(str);
-//		println(num);
-//	}
-//
-//	private int stringToInteger(String str) {
-//		for(int i = 0; i < str.length(); i++){
-//			char currCh = str.charAt(i);
-//			int currDigit = currCh - '0';
-//			num = num * 10 + currDigit;
-//		}
-//		return num;
-//	
+		String str = "3456";
+		int num = stringToInteger(str);
+		println(num);
+	}
+
+	private int stringToInteger(String str) {
+		int k = 1;
+		for(int i = 0; i < str.length(); i++){
+			char currCh = str.charAt(i);
+			int currDigit = currCh - '0';
+			k = k * 10 + currDigit;
+		}
+		return k;
+	
 	}
 }
