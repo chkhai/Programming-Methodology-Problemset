5bb3463cbcda069b7a7fe158c9bf38173150a029
S <s>
2023-12-07 17:51:45 +0400

    {"types":"C","runs":1}

--- Problem56.java
+++ Problem56.java
@@ -1,4 +1,36 @@
+import acm.program.ConsoleProgram;
 
-public class Problem56 {
+//კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, დაალაგეთ რიცხვები
+//ზრდადობით და დაბეჭდეთ.
 
+public class Problem56 extends ConsoleProgram{
+
+	public void run() {
+		int k = readInt("Enter number: ");
+		int[] arr = new int[k];
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = readInt("Enter number: ");
+		}
+		sort(arr);
+	}
+
+	private void sort(int[] arr) {
+		int min = arr[0];
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] < min){
+				swap(min, arr, i);
+			}
+			
+			
+		}
+		
+	}
+
+	private void swap(int min, int[] arr, int i) {
+		int temp = min;
+		min = arr[i];
+		arr[i] = temp;
+		
+	}
+	
 }
