4c6e1b6a28bddcf36fc464a48f1a646105e49273
S <s>
2023-12-07 17:14:19 +0400

    {"types":"C","runs":0}

--- Problem53.java
+++ Problem53.java
@@ -1,4 +1,25 @@
+import java.io.BufferedReader;
 
-public class Problem53 {
+import acm.program.ConsoleProgram;
 
+public class Problem53 extends ConsoleProgram{
+	public void run() {
+		
+//		try{
+//			BufferedReader rd = new BufferedReader(FileReader());
+//			String text = "";
+//			
+//			while(true){
+//				String line = rd.readLine();
+//				if(line == null){
+//					break;
+//				}
+//				text += line;
+//			}
+//			
+//		} catch(IOExeption e) {
+//			
+//		}
+//		
+		}
 }
