fa0e1472632a19ea464b65872ec264b6dfae282f
S <s>
2023-12-09 14:13:40 +0400

    {"types":"C","runs":7}

--- Problem55.java
+++ Problem55.java
@@ -5,11 +5,22 @@ public class Problem55 extends ConsoleProgram{
 	public void run() {
 		int k = readInt("Enter natural numer: ");
 		k = checkTheInput(k);
-		
-		
-		
+		int[] arr = new int[k];
+		if(arrIsSorted(arr)){
+			println("Array is sorted!");
+		}else{
+			println("Array is not sorted!");
+		}	
 	}
 	
+	private boolean arrIsSorted(int[] arr){
+		for(int i = 0; i < arr.length - 1; i++){
+			if(arr[i] > arr[i + 1]){
+				return false;
+			}
+		}
+		return true;
+	}
 	
 	private int checkTheInput(int k) {
 		while(k <= 0){
