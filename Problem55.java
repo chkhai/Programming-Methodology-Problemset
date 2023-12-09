dd98e7d622fa7d62897f2e97f641dcd7b0d2696c
S <s>
2023-12-09 14:10:54 +0400

    {"types":"C","runs":7}

--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem55 {
+public class Problem55 extends ConsoleProgram{
 
+	public void run() {
+		int k = readInt("Enter natural numer: ");
+		k = checkTheInput(k);
+		
+		
+		
+	}
+	
+	
+	private int checkTheInput(int k) {
+		while(k <= 0){
+			println("Number must be positive!");
+			k = readInt("Enter natural number: ");
+		}
+		return k;
+	}
 }
