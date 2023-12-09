7d58c34064e6529c16ce7025aa916017c5d81985
S <s>
2023-12-09 14:06:03 +0400

    {"types":"C","runs":6}

--- Problem54.java
+++ Problem54.java
@@ -8,13 +8,17 @@ public class Problem54 extends ConsoleProgram{
 	public void run() {
 		
 		int k = readInt("Enter number:");
-		while(k <= 0){
-			println("Number must be positive!");
-			k = readInt("Enter number");
-		}
+		k = checkTheInput(k);
 		int[] nums = new int[k];
 		int max = nums[0];
 		int min = nums[1];
+		detectMinAndMax(nums, min, max);
+		println("Maximum number is " + max);
+		println("Minimum number is " + min);
+		
+	}
+
+	private void detectMinAndMax(int[] nums, int max, int min) {
 		for(int i = 0; i < nums.length; i++){
 			nums[i] = readInt("Enter number:");
 			if(nums[i] > max){
@@ -24,8 +28,14 @@ public class Problem54 extends ConsoleProgram{
 			}
 			
 		}
-		println("Maximum number is " + max);
-		println("Minimum number is " + min);
 		
+	}
+
+	private int checkTheInput(int k) {
+		while(k <= 0){
+			println("Number must be positive!");
+			k = readInt("Enter number");
+		}
+		return k;
 	}
 }
