9be80140dbb0243a9c44425ce75b2c91bcce2918
S <s>
2023-12-12 17:26:15 +0400

    {"types":"C","runs":1}

--- Problem60.java
+++ Problem60.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
-public class Problem60 {
+import acm.program.ConsoleProgram;
 
+//კონსოლიდან შეგვყავს რიცხვები მანამ სანამ არ შეიყვანთ -1-ს, დაბეჭდეთ შეყვანილი
+//რიცხვები შებრუნებული მიმდევრობით.
+
+public class Problem60 extends ConsoleProgram{
+	
+	private static final int SENTINEL = -1;
+	
+	public void run() {
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		while(true){
+			int k = readInt("Enter number: ");
+			if(k == SENTINEL){
+				break;
+			}
+			arrayList.add(k);
+		}
+		reverseArray();
+		
+	}
+
+	private void reverseArray() {
+		
+		
+	}
+	
+	
 }
