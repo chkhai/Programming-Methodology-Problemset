6d04947476b3b5fe2a63c0adf33882bbee0c84a0
S <s>
2023-12-12 17:15:55 +0400

    {"types":"C","runs":1}

--- Problem57.java
+++ Problem57.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
 
-public class Problem57 {
+//დაწერეთ მეთოდი, რომელსაც გადაეცემა ორი სტრინგი და აბრუნებს true-ს თუკი ეს ორის
+//სტრინგი ერთმანეთის ანაგრამაა, ხოლო წინააღმდე შემთხვევაში აბრუნებს false-ს. ჩათვალეთ,
+//რომ ორივე სტრინგი მხოლოდ ლათინური ალფავიტის პატარა ასოებისგან შედგება.
+//იმპლემენტაცია გააკეთეთ მასივების გამოყენებით შემდეგი ალგორითმით: თითოეული
+//სიტყვისთვის დათვალეთ თითოეული სიმბოლოს რაოდენობა(‘a’-‘z’) და შეინახეთ მასივში.
+//შემდეგ შეადარეთ ერთმანეთს მასივები.
 
+public class Problem57 extends ConsoleProgram{
+
+	public void run(){
+		String str1 = "mariam";
+		String str2 = "mraiam";
+		println(areAnagrams(str1, str2));
+	}
+
+	private boolean areAnagrams(String str1, String str2) {
+		int[] count1= new int[26];
+		int[] count2= new int[26];
+		
+		for(int i = 0; i < str1.length(); i++){
+			char ch = str1.charAt(i);
+			int index = ch - 'a';
+		}
+		
+		
+		
+		return false;
+	}
+	
+	
 }
